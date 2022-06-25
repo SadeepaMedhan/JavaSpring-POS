311948aaa1d0cfe242a4ee859b8328197ed1e021
SadeepaMedhan <sadeepamedhan823@gmail.com>
2022-06-25 15:08:34 +0530

    added configurations

---
+++ src/main/java/lk/ijse/spring/service/CustomerService.java
@@ -0,0 +1,13 @@
+package lk.ijse.spring.service;
+
+import lk.ijse.spring.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    void saveCustomer(Customer entity);
+    void deleteCustomer(String id);
+    void updateCustomer(Customer entity);
+    Customer searchCustomer(String id);
+    List<Customer> getAllCustomers();
+}
